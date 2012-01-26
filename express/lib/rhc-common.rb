cd4b1432f5847738061aafe46ccd04800d3ffd7f
Abhishek Gupta <abhgupta@redhat.com>
2012-01-26 15:53:16 -0800

    rolling back change to convert application and namespace name to lowercase

--- express/lib/rhc-common.rb
+++ express/lib/rhc-common.rb
@@ -1,4 +1,4 @@
-# Copyright 2010 Red Hat, Inc.
+# Copyright 2011 Red Hat, Inc.
 #
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation files
