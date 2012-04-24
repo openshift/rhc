4e9728740f19b2b40690e244be901b52f4c31da6
Fotios Lindiakos <fotios@redhat.com>
2012-04-24 13:12:18 -0400

    Added rake as a dependency, so extension building will succeed Added rescue around native json installation and fallback to json_pure
    https://bugzilla.redhat.com/show_bug.cgi?id=814811

--- express/ext/mkrf_conf.rb
+++ express/ext/mkrf_conf.rb
@@ -16,7 +16,13 @@ unless ENV['RHC_RPMBUILD']
     if ENV['JSON_PURE'] or (RUBY_VERSION == "1.8.6" or RUBY_PLATFORM =~ /mswin/ or RUBY_PLATFORM =~ /darwin/)
       inst.install('json_pure')
     else
-      inst.install('json')
+      # rescue native json installation failure and fall back to json_pure
+      #   This may happen, for instance if an Ubuntu user does not have ruby-dev
+      begin
+        inst.install('json')
+      rescue
+        inst.install('json_pure')
+      end
     end
 
   rescue
