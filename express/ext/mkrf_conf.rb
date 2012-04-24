090a3781d564eb68aeb548ea1b48c2d444e69150
Fotios Lindiakos <fotios@redhat.com>
2012-04-24 15:05:32 -0400

    Added ability to remove rhc-rest when this gem gets installed This should prevent any conflicts between old rhc-rest and new libs/rhc-rest*

--- express/ext/mkrf_conf.rb
+++ express/ext/mkrf_conf.rb
@@ -1,8 +1,11 @@
 ## Add some testing for when we are building a rpm and when we're not
+msg = ''
+
 unless ENV['RHC_RPMBUILD']
   require 'rubygems'
   require 'rubygems/command.rb'
   require 'rubygems/dependency_installer.rb' 
+  require 'rubygems/uninstaller.rb'
   begin
     Gem::Command.build_args = ARGV
   rescue NoMethodError
@@ -28,8 +31,32 @@ unless ENV['RHC_RPMBUILD']
   rescue
     exit(1)
   end 
+
+  # Remove rhc-rest if it's installed
+  begin
+    rest = Gem::Specification.find_by_name('rhc-rest')
+    remove = Gem::Uninstaller.new('rhc-rest')
+    remove.uninstall
+
+    # This only gets printed if verbose is specified 
+    #   TODO: Need to figure out how to get it always shown
+    msg = <<-MSG
+      ===================================================
+        rhc-rest is no longer needed as an external gem
+          - If it is installed, it will be removed
+          - Its libraries are now included in rhc
+            - Any applications requiring rhc-rest will 
+              still function as expected
+      ===================================================
+    MSG
+
+
+  rescue Gem::LoadError
+  end
 end
 
 f = File.open(File.join(File.dirname(__FILE__), "Rakefile"), "w")   # create dummy rakefile to indicate success
-f.write("task :default\n")
+f.write("task :default do\n")
+f.write("\tputs '%s'\n" % msg) if msg
+f.write("end\n")
 f.close
