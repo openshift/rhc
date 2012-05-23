9559c99cc7d8c841abc6d48a593bbf6e01fa53b5
Fabiano Franz <ffranz@redhat.com>
2012-05-23 19:49:51 -0300

    Improved rspec syntax

--- express/spec/rhc/json_spec.rb
+++ express/spec/rhc/json_spec.rb
@@ -4,26 +4,24 @@ require 'rhc/json'
 describe RHC::Json do
 
   context 'with simple decoded hash as a string' do
-    before { @decoded = RHC::Json.decode '{"abc":[123,-456.789e0],"def":[456,-456.789e0],"ghi":"ghj"}' }
-    it('should contain all elements') { @decoded.length.should == 3  }
-    it('should contain key') { @decoded.has_key?("abc").should == true  }
-    it('should contain key') { @decoded.has_key?("def").should == true  }
-    it('should contain key') { @decoded.has_key?("ghi").should == true  }
-    it('should not contain invalid key') { @decoded.has_key?("ghj").should == false  }
-    it('should contain value for key') { @decoded.has_value?("ghj").should == true  }
-    it('should contain array value') { @decoded["abc"].is_a?(Array).should == true  }
-    it('should contain array with two elements') { @decoded["abc"].length.should == 2  }
-    it('should contain array with an integer') { @decoded["abc"][0].should == 123  }
-    it('should contain array with a float') { @decoded["abc"][1].should == -456.789e0 }
-    after { @decoded = nil }
+    subject { RHC::Json.decode '{"abc":[123,-456.789e0],"def":[456,-456.789e0],"ghi":"ghj"}' }
+    its(:length) { should == 3 }
+    it('should contain key') { subject.has_key?("abc").should be_true  }
+    it('should contain key') { subject.has_key?("def").should be_true  }
+    it('should contain key') { subject.has_key?("ghi").should be_true  }
+    it('should not contain invalid key') { subject.has_key?("ghj").should be_false  }
+    it('should contain value for key') { subject.has_value?("ghj").should be_true  }
+    it('should contain array value') { subject["abc"].is_a?(Array).should be_true  }
+    it('should contain array with two elements') { subject["abc"].length.should == 2  }
+    it('should contain array with an integer') { subject["abc"][0].should == 123  }
+    it('should contain array with a float') { subject["abc"][1].should == -456.789e0 }
   end
 
   context 'with simple hash' do
-    before { @encoded = RHC::Json.encode({"key" => "value"}) }
-    it('should encode to proper json') { @encoded.should == '{"key":"value"}'  }
-    it('should encode and decode to the same hash') { RHC::Json.decode(@encoded).should == {"key" => "value"} }
+    subject { RHC::Json.encode({"key" => "value"}) }
+    it('should encode to proper json') { subject.should == '{"key":"value"}'  }
+    it('should encode and decode to the same hash') { RHC::Json.decode(subject).should == {"key" => "value"} }
     it('should decode and encode to the same string') { RHC::Json.encode(RHC::Json.decode('{"x":"y"}')).should == '{"x":"y"}' }
-    after { @encoded = nil }
   end
 
 end
--- express/spec/rhc/targz_spec.rb
+++ express/spec/rhc/targz_spec.rb
@@ -4,10 +4,10 @@ require 'rhc/targz'
 describe RHC::TarGz do
 
   context 'with simple compressed .tar.gz' do
-    before { @filename = File.expand_path('../assets/targz_sample.tar.gz', __FILE__) }
-    it('should contain the right files') { RHC::TarGz.contains(@filename, /foo/).should == true }
-    it('should contain the right files') { RHC::TarGz.contains(@filename, /bar/).should == false }
-    after { @filename = nil }
+    subject { File.expand_path('../assets/targz_sample.tar.gz', __FILE__) }
+    it('should wrap the right filename') { File.basename(subject).should ==  'targz_sample.tar.gz' }
+    it('should contain the right files') { RHC::TarGz.contains(subject, /foo/).should be_true }
+    it('should contain the right files') { RHC::TarGz.contains(subject, /bar/).should be_false }
   end
 
 end
