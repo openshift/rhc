4341a0168e3f619ccfd6a588f05b4495ec9725c5
Fabiano Franz <ffranz@redhat.com>
2012-05-24 20:16:39 -0300

    Removed pr-zlib in favour of zliby to solve pr-zlib multithreading bug, added more rspec tests

--- express/lib/rhc/targz.rb
+++ express/lib/rhc/targz.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'stringio'
-require 'vendor/pr/zlib'
+require 'vendor/zliby'
 require 'archive/tar/minitar'
 include Archive::Tar
 
@@ -9,17 +9,18 @@ module RHC
 	module TarGz
 
     def self.contains(filename, search)
+      return false if ! (File.file? filename and File.basename(filename).downcase =~ /.\.tar\.gz$/i)
       search = /#{search.to_s}/ if ! search.is_a?(Regexp)
       contains = false
       begin
-        Rhc::Vendor::Zlib::GzipReader.open(filename) do |gzip|
-          tar = Minitar::Reader.new(gzip)
-          tar.each_entry do |entry|
-            if entry.full_name =~ search
-              contains = true
+        Rhc::Vendor::Zlib::GzipReader.open(filename) do |gz|
+          Minitar::Reader.open gz do |tar|
+            tar.each_entry do |entry|
+              if entry.full_name =~ search
+                contains = true
+              end
             end
           end
-          tar.close
         end
       rescue Rhc::Vendor::Zlib::GzipFile::Error
         return false
