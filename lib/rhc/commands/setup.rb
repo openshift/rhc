1bc6cbfab85aa9a2ced4607b50c1426adf194193
John (J5) Palmieri <johnp@redhat.com>
2012-07-18 16:28:33 -0400

    remove help handler in setup command as we handle it generically now

--- lib/rhc/commands/setup.rb
+++ lib/rhc/commands/setup.rb
@@ -8,12 +8,6 @@ module RHC::Commands
     summary "Runs the setup wizard to configure your OpenShift account."
     suppress_wizard
     def run
-      # TODO: make help subcommand global
-      if args[0] == 'help'
-        say Commander::Runner.instance.help_formatter.render_command(@command)
-        return 0
-      end
-
       w = RHC::RerunWizard.new(config.config_path)
       # exit 0 on success 1 otherwise
       w.run ? 0 : 1
