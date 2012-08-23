b59a97029def0d9ac9fa16b3e86cbc8c8ec99f6e
Adam Miller <admiller@redhat.com>
2012-08-22 19:28:14 -0500

    bump_minor_versions for sprint 17

--- lib/rhc/version.rb
+++ lib/rhc/version.rb
@@ -1,8 +1,8 @@
 module RHC
   module VERSION #:nocov:
     MAJOR = 0
-    MINOR = 97
-    MICRO = 16
+    MINOR = 98
+    MICRO = 0
     #PRE  = ''
     STRING = [MAJOR,MINOR,MICRO].compact.join('.')
   end
