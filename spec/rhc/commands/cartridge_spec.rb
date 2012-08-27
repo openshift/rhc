b56d5aa26d687b65c83c9a6154969c25f8c416fe
John (J5) Palmieri <johnp@redhat.com>
2012-08-27 11:59:53 -0400

    Revert "remove cartridge spec so we can merge with master"
    This reverts commit a4cc1e1d795e9e7789476ddfcbd6a2604dbff7fb.

---
+++ spec/rhc/commands/cartridge_spec.rb
@@ -0,0 +1,88 @@
+require 'spec_helper'
+require 'rest_spec_helper'
+require 'rhc/commands/cartridge'
+require 'rhc/config'
+
+describe RHC::Commands::Cartridge do
+  before(:each) do
+    RHC::Config.set_defaults
+  end
+
+  describe 'run' do
+    let(:arguments) { ['cartridge', '--trace', '--noprompt', '--config', 'test.conf', '-l', 'test@test.foo', '-p',  'password'] }
+
+    context 'when run' do
+      before(:each) do
+        @rc = MockRestClient.new
+      end
+      it { expect { run }.should exit_with_code(0) }
+      it { run_output.should match("mock_cart-1, mock_cart-2, unique_mock_cart-1") }
+    end
+  end
+
+  describe 'alias app cartridge' do
+    let(:arguments) { ['app', 'cartridge', '--noprompt', '--config', 'test.conf', '-l', 'test@test.foo', '-p',  'password'] }
+
+    context 'when run' do
+      before(:each) do
+        @rc = MockRestClient.new
+      end
+      it { expect { run }.should exit_with_code(0) }
+      it { run_output.should match("mock_cart-1, mock_cart-2, unique_mock_cart-1") }
+    end
+  end
+
+  describe 'cartridge add' do
+    let(:arguments) { ['cartridge', 'add', 'mock_cart-1', '--app', 'app1', '--noprompt', '--config', 'test.conf', '-l', 'test@test.foo', '-p',  'password'] }
+
+    context 'when run' do
+      before(:each) do
+        @rc = MockRestClient.new
+        domain = @rc.add_domain("mock_domain")
+        app = domain.add_application("app1", "mock_type")
+      end
+      it { expect { run }.should exit_with_code(0) }
+      it { run_output.should match("Success") }
+    end
+  end
+
+  describe 'alias app cartridge add' do
+    let(:arguments) { ['app', 'cartridge', 'add', 'unique_mock_cart', '--app', 'app1', '--noprompt', '--config', 'test.conf', '-l', 'test@test.foo', '-p',  'password'] }
+
+    context 'when run' do
+      before(:each) do
+        @rc = MockRestClient.new
+        domain = @rc.add_domain("mock_domain")
+        app = domain.add_application("app1", "mock_type")
+      end
+      it { expect { run }.should exit_with_code(0) }
+      it { run_output.should match("Success") }
+    end
+  end
+
+  describe 'cartridge add no cart found error' do
+    let(:arguments) { ['cartridge', 'add', 'nomatch_cart', '--app', 'app1', '--noprompt', '--config', 'test.conf', '-l', 'test@test.foo', '-p',  'password'] }
+
+    context 'when run' do
+      before(:each) do
+        @rc = MockRestClient.new
+        domain = @rc.add_domain("mock_domain")
+        app = domain.add_application("app1", "mock_type")
+      end
+      it { expect { run }.should exit_with_code(154) }
+    end
+  end
+
+  describe 'cartridge add too many carts found error' do
+    let(:arguments) { ['cartridge', 'add', 'mock_cart', '-a', 'app1','--noprompt', '--config', 'test.conf', '-l', 'test@test.foo', '-p',  'password'] }
+
+    context 'when run' do
+      before(:each) do
+        @rc = MockRestClient.new
+        domain = @rc.add_domain("mock_domain")
+        app = domain.add_application("app1", "mock_type")
+      end
+      it { expect { run }.should exit_with_code(155) }
+    end
+  end
+end
