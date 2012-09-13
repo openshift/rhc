d1fb67845bef86c678ad2815ff33033631544e61
John (J5) Palmieri <johnp@redhat.com>
2012-09-12 23:13:01 -0400

    fixed some typos and add debug spew to app context

--- lib/rhc/context_helper.rb
+++ lib/rhc/context_helper.rb
@@ -1,18 +1,22 @@
 module RHC
   module ContextHelpers
     def app_context
+      debug "Getting app context"
       get_uuid_cmd = "git config --get rhc.app-uuid"
+      debug "Running #{get_uuid_cmd}"
       uuid = %x[#{get_uuid_cmd}].strip
-      return nil if $?.exitstatus != 0 or output.empty?
+      debug "UUID = '#{uuid}'"
+      return nil if $?.exitstatus != 0 or uuid.empty?
 
       # proof of concept - we shouldn't be traversing
       # the broker should expose apis for getting the application via a uuid
       rest_client.domains.each do |rest_domain|
-        rest_domain.applications do |rest_app|
+        rest_domain.applications.each do |rest_app|
           return rest_app.name if rest_app.uuid == uuid
         end
       end
 
+      debug "Couldn't find app with UUID == #{uuid}"
       nil
     end
 
