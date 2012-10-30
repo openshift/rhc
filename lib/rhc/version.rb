c702ea482a47d2b455c8b431cc7346bcfe36350e
Dan McPherson <dmcphers@redhat.com>
2012-10-29 20:51:31 -0400

    bumping spec to 1.0.0

--- lib/rhc/version.rb
+++ lib/rhc/version.rb
@@ -1,8 +1,8 @@
 module RHC
   module VERSION #:nocov:
-    MAJOR = 0
-    MINOR = 99
-    MICRO = 14
+    MAJOR = 1
+    MINOR = 0
+    MICRO = 0
     #PRE  = ''
     STRING = [MAJOR,MINOR,MICRO].compact.join('.')
   end
