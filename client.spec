cc0325045502912520c5451300ada7138b586b6d
Adam Miller <admiller@redhat.com>
2012-11-01 19:14:20 -0500

    bump_minor_versions for sprint 20

--- client.spec
+++ client.spec
@@ -3,7 +3,7 @@
 
 Summary:       OpenShift client management tools
 Name:          rhc
-Version: 1.0.4
+Version: 1.1.0
 Release:       1%{?dist}
 Group:         Network/Daemons
 License:       ASL 2.0
--- lib/rhc/version.rb
+++ lib/rhc/version.rb
@@ -1,8 +1,8 @@
 module RHC
   module VERSION #:nocov:
     MAJOR = 1
-    MINOR = 0
-    MICRO = 4
+    MINOR = 1
+    MICRO = 0
     #PRE  = ''
     STRING = [MAJOR,MINOR,MICRO].compact.join('.')
   end
