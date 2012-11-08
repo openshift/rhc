2393f12ed458839fc17a130253eea6bb7acac3c0
Hiro Asari <asari.ruby@gmail.com>
2012-11-08 09:57:23 -0500

    Refactor common code in RSpec examples.

--- spec/rhc/commands/port-forward_spec.rb
+++ spec/rhc/commands/port-forward_spec.rb
@@ -12,15 +12,18 @@ describe RHC::Commands::PortForward do
   describe 'run' do
     let(:arguments) { ['port-forward', '--noprompt', '--config', 'test.conf', '-l', 'test@test.foo', '-p', 'password', '--app', 'mockapp'] }
 
+    before :each do
+      @rc = MockRestClient.new
+      @domain = @rc.add_domain("mockdomain")
+      @app = @domain.add_application 'mockapp', 'mock-1.0'
+      @uri = URI.parse @app.ssh_url
+      @ssh = mock(Net::SSH)
+    end
+
     context 'when port forwarding an app without ports to forward' do
       before(:each) do
-        @rc = MockRestClient.new
-        domain = @rc.add_domain("mockdomain")
-        app = domain.add_application 'mockapp', 'mock-1.0'
-        uri = URI.parse app.ssh_url
-        ssh = mock(Net::SSH)
-        Net::SSH.should_receive(:start).with(uri.host, uri.user).and_yield(ssh)
-        ssh.should_receive(:exec!).with("rhc-list-ports").and_yield(nil, :stderr, '127.0.0.1:3306')
+        Net::SSH.should_receive(:start).with(@uri.host, @uri.user).and_yield(@ssh)
+        @ssh.should_receive(:exec!).with("rhc-list-ports").and_yield(nil, :stderr, '127.0.0.1:3306')
       end
       it "should error out as no ports to forward" do
         expect { run }.should exit_with_code(102)
@@ -33,13 +36,8 @@ describe RHC::Commands::PortForward do
 
     context 'when port forwarding an app with permission denied ports' do
       before(:each) do
-        @rc = MockRestClient.new
-        domain = @rc.add_domain("mockdomain")
-        app = domain.add_application 'mockapp', 'mock-1.0'
-        ssh = mock(Net::SSH)
-        uri = URI.parse app.ssh_url
-        Net::SSH.should_receive(:start).with(uri.host, uri.user).and_yield(ssh)
-        ssh.should_receive(:exec!).with("rhc-list-ports").and_yield(nil, :stderr, 'permission denied')
+        Net::SSH.should_receive(:start).with(@uri.host, @uri.user).and_yield(@ssh)
+        @ssh.should_receive(:exec!).with("rhc-list-ports").and_yield(nil, :stderr, 'permission denied')
       end
       it "should error out as permission denied" do
         expect { run }.should exit_with_code(129)
@@ -52,21 +50,16 @@ describe RHC::Commands::PortForward do
 
     context 'when port forwarding an app with ports to forward' do
       before(:each) do
-        @rc = MockRestClient.new
-        domain = @rc.add_domain("mockdomain")
-        app = domain.add_application 'mockapp', 'mock-1.0'
-        ssh = mock(Net::SSH)
-        uri = URI.parse app.ssh_url
-        Net::SSH.should_receive(:start).with(uri.host, uri.user).and_yield(ssh).twice
-        ssh.should_receive(:exec!).with("rhc-list-ports").and_yield(nil, :stderr, 'mysql -> 127.0.0.1:3306')
+        Net::SSH.should_receive(:start).with(@uri.host, @uri.user).and_yield(@ssh).twice
+        @ssh.should_receive(:exec!).with("rhc-list-ports").and_yield(nil, :stderr, 'mysql -> 127.0.0.1:3306')
         forward = mock(Net::SSH::Service::Forward)
-        ssh.should_receive(:forward).and_return(forward)
+        @ssh.should_receive(:forward).and_return(forward)
         if mac?
           forward.should_receive(:local).with(3306, '127.0.0.1', 3306)
         else
           forward.should_receive(:local).with('127.0.0.1', 3306, '127.0.0.1', 3306)
         end
-        ssh.should_receive(:loop)
+        @ssh.should_receive(:loop)
       end
       it "should run successfully" do
         expect { run }.should exit_with_code(0)
@@ -79,11 +72,6 @@ describe RHC::Commands::PortForward do
 
     context 'when host is unreachable' do
       before(:each) do
-        @rc = MockRestClient.new
-        domain = @rc.add_domain("mockdomain")
-        app = domain.add_application 'mockapp', 'mock-1.0'
-        ssh = mock(Net::SSH)
-        uri = URI.parse app.ssh_url
         Net::SSH.should_receive(:start).and_raise(Errno::EHOSTUNREACH)
       end
       it "should error out" do
@@ -97,21 +85,16 @@ describe RHC::Commands::PortForward do
 
     context 'when port forwarding an app with ports to forward' do
       before(:each) do
-        @rc = MockRestClient.new
-        domain = @rc.add_domain("mockdomain")
-        app = domain.add_application 'mockapp', 'mock-1.0'
-        ssh = mock(Net::SSH)
-        uri = URI.parse app.ssh_url
-        Net::SSH.should_receive(:start).with(uri.host, uri.user).and_yield(ssh).twice
-        ssh.should_receive(:exec!).with("rhc-list-ports").and_yield(nil, :stderr, 'mysql -> 127.0.0.1:3306')
+        Net::SSH.should_receive(:start).with(@uri.host, @uri.user).and_yield(@ssh).twice
+        @ssh.should_receive(:exec!).with("rhc-list-ports").and_yield(nil, :stderr, 'mysql -> 127.0.0.1:3306')
         forward = mock(Net::SSH::Service::Forward)
-        ssh.should_receive(:forward).and_return(forward)
+        @ssh.should_receive(:forward).and_return(forward)
         if mac?
           forward.should_receive(:local).with(3306, '127.0.0.1', 3306)
         else
           forward.should_receive(:local).with('127.0.0.1', 3306, '127.0.0.1', 3306)
         end
-        ssh.should_receive(:loop).and_raise(Interrupt.new)
+        @ssh.should_receive(:loop).and_raise(Interrupt.new)
       end
       it "should exit when user interrupts" do
         expect { run }.should exit_with_code(0)
@@ -124,15 +107,10 @@ describe RHC::Commands::PortForward do
 
     context 'when host refuses connection' do
       before(:each) do
-        @rc = MockRestClient.new
-        domain = @rc.add_domain("mockdomain")
-        app = domain.add_application 'mockapp', 'mock-1.0'
-        ssh = mock(Net::SSH)
-        uri = URI.parse app.ssh_url
-        Net::SSH.should_receive(:start).with(uri.host, uri.user).and_yield(ssh).twice
-        ssh.should_receive(:exec!).with("rhc-list-ports").and_yield(nil, :stderr, 'mysql -> 127.0.0.1:3306')
+        Net::SSH.should_receive(:start).with(@uri.host, @uri.user).and_yield(@ssh).twice
+        @ssh.should_receive(:exec!).with("rhc-list-ports").and_yield(nil, :stderr, 'mysql -> 127.0.0.1:3306')
         forward = mock(Net::SSH::Service::Forward)
-        ssh.should_receive(:forward).and_raise(Errno::ECONNREFUSED)
+        @ssh.should_receive(:forward).and_raise(Errno::ECONNREFUSED)
       end
       it "should error out" do
         expect { run }.should exit_with_code(0)
@@ -143,15 +121,10 @@ describe RHC::Commands::PortForward do
 
     context 'when port forwarding a scaled app with ports to forward' do
       before(:each) do
-        @rc = MockRestClient.new
-        domain = @rc.add_domain("mockdomain")
-        app = domain.add_application 'mockapp', 'mock-1.0'
-        ssh = mock(Net::SSH)
-        uri = URI.parse app.ssh_url
-        Net::SSH.should_receive(:start).with(uri.host, uri.user).and_yield(ssh).twice
-        ssh.should_receive(:exec!).with("rhc-list-ports").and_yield(nil, :stderr, "httpd -> 127.0.0.1:8080\nhttpd -> 127.0.0.2:8080")
+        Net::SSH.should_receive(:start).with(@uri.host, @uri.user).and_yield(@ssh).twice
+        @ssh.should_receive(:exec!).with("rhc-list-ports").and_yield(nil, :stderr, "httpd -> 127.0.0.1:8080\nhttpd -> 127.0.0.2:8080")
         forward = mock(Net::SSH::Service::Forward)
-        ssh.should_receive(:forward).at_least(3).times.and_return(forward)
+        @ssh.should_receive(:forward).at_least(3).times.and_return(forward)
         if mac?
           forward.should_receive(:local).with(8080, '127.0.0.1', 8080)
           forward.should_receive(:local).with(8080, '127.0.0.2', 8080).and_raise(Errno::EADDRINUSE)
@@ -161,7 +134,7 @@ describe RHC::Commands::PortForward do
           forward.should_receive(:local).with('127.0.0.2', 8080, '127.0.0.2', 8080).and_raise(Errno::EADDRINUSE)
           forward.should_receive(:local).with('127.0.0.2', 8081, '127.0.0.2', 8080)
         end
-        ssh.should_receive(:loop).and_raise(Interrupt.new)
+        @ssh.should_receive(:loop).and_raise(Interrupt.new)
       end
       it "should exit when user interrupts" do
         expect { run }.should exit_with_code(0)
@@ -172,6 +145,6 @@ describe RHC::Commands::PortForward do
       it { run_output.should include("Ending port forward") }
     end
 
-   end
+  end
 end
 
