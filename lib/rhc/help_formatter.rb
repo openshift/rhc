7371bb1f870cf0418209c1454bb635556a9d023a
Clayton Coleman <ccoleman@redhat.com>
2012-11-17 21:06:24 -0500

    Bug 876715 - Users don't know what cart types to add.

--- lib/rhc/help_formatter.rb
+++ lib/rhc/help_formatter.rb
@@ -5,6 +5,9 @@ module RHC
     def template(name)
       ERB.new(File.read(File.join(File.dirname(__FILE__), 'usage_templates', "#{name}.erb")), nil, '-')
     end
+    def render_command_syntax command
+      template(:command_syntax_help).result command.get_binding
+    end
   end
   # TODO: class ManPageHelpFormatter
 end
