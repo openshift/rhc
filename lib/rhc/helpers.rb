9b6c8a7ac2b940cdd00942208d7b6baaf70c8479
Hiro Asari <asari.ruby@gmail.com>
2013-01-08 12:10:53 -0500

    US2410 roll 'rhc-chk checks into "rhc setup"'

--- lib/rhc/helpers.rb
+++ lib/rhc/helpers.rb
@@ -253,6 +253,15 @@ module RHC
     def pluralize(count, s)
       count == 1 ? "#{count} #{s}" : "#{count} #{s}s"
     end
+    
+    ## report a result (true/false) and return result
+    ## if the result is false, msg is displayed
+    def report_result(result, msg, fatal = true)
+      result ? success('.') : error(msg)
+      
+      # ignore the result if non-fatal
+      fatal ? result : true
+    end
 
     # given an array of arrays "items", construct an array of strings that can
     # be used to print in tabular form.
